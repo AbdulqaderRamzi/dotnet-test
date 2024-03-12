f914a743e0c326cfbca73462a7ae1cbcb3d314b4
Abdulqader <3boodg9@gmail.com>
2024-03-12 22:52:51 +0300

    fix-datatable

---
+++ CRUDHistory.DataAccess/Repository/SolutionRepository.cs
@@ -0,0 +1,14 @@
+﻿using CRUDHistory.DataAccess.Data;
+using CRUDHistory.DataAccess.Repository.IRepository;
+using CRUDHistory.Models.Models;
+
+namespace CRUDHistory.DataAccess.Repository;
+
+public class SolutionRepository : Repository<Solution>, ISolutionRepository{
+    private readonly ApplicationDbContext _db;
+    public SolutionRepository(ApplicationDbContext db) : base(db){
+        _db = db;
+    }
+
+    public void Update(Solution solution) => _db.Update(solution);
+}
--- CRUDHistoryWeb/obj/Debug/net8.0/CRUDHistoryWeb.AssemblyInfo.cs
+++ CRUDHistoryWeb/obj/Debug/net8.0/CRUDHistoryWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDHistoryWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff0c435e05323e588a20318cda07cd97509cacf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDHistoryWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDHistoryWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
